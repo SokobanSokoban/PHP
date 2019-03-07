5d34a5dfe6e91c4f5d56765c7f310e1819bdfa6d
sokobansokoban <ottobrall@gmail.com>
2019-03-07 22:13:47 +0300

    les5

---
+++ level 1/les5/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php require 'log.php'; ?>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <h1>Галерея</h1>
+
+    <div class="container">
+
+        <?php require 'gallery.php'; ?>
+
+    </div>
+
+    <?php require 'footer.php'; ?>
+</body>
+
+</html>
